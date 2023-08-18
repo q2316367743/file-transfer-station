ee7c87470d1b654b5518a05b0d8bcd2b2afb240f
落雨不悔 <m17762618644@163.com>
2023-08-18 12:04:27 +0800

    项目初始化

---
+++ preload.js
@@ -0,0 +1,37 @@
+window.exports = {
+    "application": {
+        mode: "none",  // 用于无需 UI 显示，执行一些简单的代码
+        args: {
+            // 进入插件应用时调用
+            enter: () => {
+                const ubWindow = utools.createBrowserWindow('index.html', {
+                    useContentSize: true,
+                    width: 200,
+                    height: 232,
+                    frame: true,
+                    transparent: false,
+                    backgroundColor: '#000000',
+                    hasShadow: false,
+                    webPreferences: {
+                        preload: 'index.js'
+                    },
+                    titleBarStyle: 'hidden',
+                    titleBarOverlay: true,
+                    alwaysOnTop: true
+                }, () => {
+                    // 设置窗口是否可以由用户手动最大化。
+                    ubWindow.setMaximizable(false)
+                    // 设置用户是否可以调节窗口尺寸
+                    ubWindow.setResizable(false)
+                    ubWindow.show();
+                    utools.hideMainWindow();
+                    if (utools.isDev()) {
+                        ubWindow.webContents.openDevTools();
+                    } else {
+                        utools.outPlugin();
+                    }
+                });
+            }
+        }
+    }
+}
