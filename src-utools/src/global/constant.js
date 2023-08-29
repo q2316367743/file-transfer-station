fe75f2e3b9e9bbab265bd562f160fd6efe421cf0
落雨不悔 <m17762618644@163.com>
2023-08-29 20:44:21 +0800

    Revert "项目优化"
    This reverts commit f5d3b34899577d2f057c2809f1eba0f1694d1521.

--- src-utools/src/global/constant.js
+++
@@ -1,8 +0,0 @@
-export const SETTING = {
-    SKIP_TASKBAR: '/setting/skipTaskbar',
-    ALWAYS_ON_TOP: '/setting/alwaysOnTop',
-    HEIGHT: '/setting/height',
-    WIDTH: '/setting/width',
-    BACKGROUND_COLOR: '/setting/backgroundColor',
-    COLOR: '/setting/color'
-}
